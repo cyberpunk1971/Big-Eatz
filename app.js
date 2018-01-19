5418a08a2df7e7ba0b9ffc9dc0cc42474a5baf09
user.name <cybertrooper1971@gmail.com>
2018-01-19 11:56:00 -0700

    intial html and js content

---
+++ app.js
@@ -0,0 +1,9 @@
+const API_KEY = "f1a96b38bc3a8c9083b0667edd9a7cad";
+const SEARCH_URL = "http://food2fork.com/api/search";
+
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    key: API_KEY,
+    q: `${searchTerm} in:name`,
+  }
+}
