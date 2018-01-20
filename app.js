dabefa5612da6595e8812e59b035bfaf2c8e3f3d
user.name <cybertrooper1971@gmail.com>
2018-01-20 14:04:36 -0700

    Getting data back to console now.

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const API_KEY = "f1a96b38bc3a8c9083b0667edd9a7cad";
-const SEARCH_URL = "http://food2fork.com/api/search";
+const API_KEY = "8f4f8a684459af2852013466a60ed3ec";
+const SEARCH_URL = "https://api.edamam.com/search";
+const APP_ID = "b5c71b52";
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
     url: SEARCH_URL,
     data: {
-      q:`${searchTerm}in:name`,
-      crossDomain:true,
-      key: API_KEY
+      q:`${searchTerm}`,
+      app_id: APP_ID,
+      app_key: API_KEY,
+      from: 0,
+      to: 5
     },
-    dataType: 'json',
+    dataType: 'jsonp',
     type: 'GET',
     success: callback,
-
-  };
-
-
-  $.ajax(query).done(function(data){console.log(data)});
+    jsonp: 'callback'
+};
+  // $.ajax(query);
+  $.getJSON(SEARCH_URL, query.data, displayData);
 
 }
 function displayData(data) {
-  const results = data.search.map((item, index) => renderResult(item));
+  data.hits.forEach(item =>{
+    console.log(item.recipe.label);
+    console.log(item.recipe.image);
+    console.log(item.recipe.ingredientLines.join(","));
+  });
 }
-getDataFromApi('steak', displayData);
 
-// function displayData(data) {
-//   const results =
-// }
+getDataFromApi('steak', displayData);
