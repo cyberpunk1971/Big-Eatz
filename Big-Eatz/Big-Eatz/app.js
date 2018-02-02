1ac4660cafc5cd6d70ae0faba917990ab186c470
Alex <adt71@cox.net>
2018-02-01 17:17:44 -0700

    added more info to landing page

--- Big-Eatz/Big-Eatz/app.js
+++ Big-Eatz/Big-Eatz/app.js
@@ -5,7 +5,6 @@ const state = {
   from: 0,
   to: 6
 };
-
 function getDataFromApi(input, callback) {
   const query = {
     url: SEARCH_URL,
@@ -15,16 +14,24 @@ function getDataFromApi(input, callback) {
       app_key: API_KEY,
       from: state.from,
       to: state.to
+
     },
+    // dataType: 'jsonp',
+    // type: 'GET',
+    // success: callback,
+    // jsonp: 'callback'
   };
+  // $.ajax(query);
   $.getJSON(SEARCH_URL, query.data, displayData);
+
 }
 
 function displayData(data) {
   $('#js-results').empty();
   data.hits.forEach(item => {
     let html = $(`<div class="container">
-      <div class="col s12 m4">
+
+        <div class="col s12 m4">
           <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
               <img class=" activator js-img" src="${item.recipe.image}">
@@ -32,10 +39,13 @@ function displayData(data) {
             </div>
             <div class="card-content">
                <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
-               </div>
+
+
+            </div>
             <div class="card-reveal">
       <span class="card-title grey-text text-darken-4"><i class="small material-icons right">close</i></span>
       <p class="card-txt">${item.recipe.dietLabels}</p>
+
       <p class="card-txt" id="card-cal">${Math.round(item.recipe.calories)} Calories</p>
       <p class="card-txt" id="card-carb">${Math.round(item.recipe.totalNutrients.CHOCDF.quantity)}<span>${item.recipe.totalNutrients.CHOCDF.unit} Carbs per serving</span></p>
         <p class="card-txt-lines">${item.recipe.ingredientLines}</p>
@@ -45,7 +55,9 @@ function displayData(data) {
             </div>
           </div>
         </div>
-        </div>`);
+
+      </div>`);
+
 
     $('#js-results').append(html);
     console.log(item.recipe.label);
@@ -53,6 +65,8 @@ function displayData(data) {
     console.log(item.recipe.ingredientLines.join(","));
     console.log(item.recipe.url);
     console.log(item.recipe.calories);
+    // ${Math.round(item.recipe.totalNutrients.CHOCDF.quantity)}
+    // ${item.recipe.totalNutrients.CHOCDF.unit}
   });
   $('.js-buttons').show();
 }
@@ -83,3 +97,47 @@ $(document).ready(function() {
     $(this).val('');
   });
 });
+
+
+
+
+// <div class="container">
+// <div class="col s12 m7 l6">
+// <div class="card">
+//   <div class="card-image waves-effect waves-block waves-light">
+//     <img class="activator" src="${item.recipe.image}">
+//   </div>
+//   <div class="card-content">
+//   <span class="card-title activator grey-text text-darken-4 col s12 m7 l6">${item.recipe.label}<i class="material-icons right col s12 m7 l6">more_vert</i></span>
+//     <p>${item.recipe.ingredientLines}</p>
+//   <p><a href="${item.recipe.url}">Try It!</a></p>
+//   </div>
+//   <div class="card-reveal">
+//   <span class="card-title grey-text text-darken-4">${item.recipe.label}<i class="material-icons right">X</i></span>
+//   <p><span id="cal-count">Calories:</span> ${Math.round(item.recipe.calories)}</p>
+//   <p><span id="cal-count">Diet:</span> ${item.recipe.dietLabels}</p>
+//   <p><span id="cal-count">Carbs: </span>
+//   ${Math.round(item.recipe.totalNutrients.CHOCDF.quantity)}<span>
+//   ${item.recipe.totalNutrients.CHOCDF.unit}
+//   </span
+//   </p>
+//   </div>
+//   </div>
+//   </div>
+// </div>
+// </div>
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getDataFromApi('chicken', displayData);
---
+++ Big-Eatz/README.md
@@ -0,0 +1,6 @@
+Big Eatz is a simple recipe search app using Edemam API. Technologies used are HTML/CSS, Javascript and jQuery. Materializecss
+is the css library for styling as well as a little flexbox for layout. USER enters a food or ingredient in search bar, and results 
+are displayed six at a time. The free version of this API is limited to 5 calls/5 minutes but up to 500/24hr period. Due to credit where
+is due consideration, the instructions for cooking are at the original site and will open in a new tab/window. 
+Live version located at https://cyberpunk1971.github.io/Big-Eatz/
+
